2aeda9fca8cacf2607b227f6bbfafc6cf13a41ee
lvidal1 <lvidal910@gmail.com>
2023-01-03 22:46:32 -0500

    Add classNames utility & add initial markup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Roboto_Mono } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import classNames from 'classnames'
 
 const roboto = Roboto_Mono({ weight: "400" })
 
@@ -13,10 +14,31 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={roboto.className}>
-        <h1 className="text-3xl font-bold tracking-wide text-center p-4">
-          Talk is cheap
-        </h1>
+      <main className={classNames([roboto.className, "text-white", "bg-dark-gray"])}>
+        <section className={classNames(["mx-auto max-w-screen-xl", "py-20", "h-screen"])}>
+
+          <div className="grid grid-cols-2 gap-4 py-20">
+            <div className='w-4/5 ml-auto space-y-4'>
+              <p className=''>Full Stack Developer</p>
+              <h1 className="text-3xl font-bold tracking-wide">
+                Talk is cheap.<br />
+                Show me the code
+              </h1>
+              <p className=''>
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+              </p>
+            </div>
+            <div>
+            </div>
+          </div>
+
+        </section>
+        <section className={classNames(["w-full", "py-20", "h-screen", "bg-red-500"])}>
+          <div className={classNames(["mx-auto max-w-screen-xl"])}>
+            Demo
+          </div>
+        </section>
+
       </main>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gray": "#2d2e32"
+      }
+    },
   },
   plugins: [],
 }
