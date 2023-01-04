fe383c008942296dd7b8ca444cedf168b38deb34
lvidal1 <lvidal910@gmail.com>
2023-01-03 23:19:12 -0500

    Add font to body

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Roboto_Mono } from '@next/font/google'
+
+const roboto = Roboto_Mono({ weight: "300" })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <style jsx global>{`
+    :root {
+      --roboto-font: ${roboto.style.fontFamily};
+    }
+  `}</style><Component {...pageProps} />
+  </>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from 'next/head'
-import { Roboto_Mono } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import classNames from 'classnames'
-
-const roboto = Roboto_Mono({ weight: "400" })
 
 export default function Home() {
   return (
@@ -14,13 +11,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={classNames([roboto.className, "text-white", "bg-dark-gray"])}>
+      <main className={classNames(["text-white", "bg-dark-gray"])}>
         <section className={classNames(["mx-auto max-w-screen-xl", "py-20", "h-screen"])}>
-
           <div className="grid grid-cols-2 gap-4 py-20">
             <div className='w-4/5 ml-auto space-y-4'>
               <p className=''>Full Stack Developer</p>
-              <h1 className="text-3xl font-bold tracking-wide">
+              <h1 className="text-5xl font-bold tracking-wide">
                 Talk is cheap.<br />
                 Show me the code
               </h1>
@@ -31,7 +27,6 @@ export default function Home() {
             <div>
             </div>
           </div>
-
         </section>
         <section className={classNames(["w-full", "py-20", "h-screen", "bg-red-500"])}>
           <div className={classNames(["mx-auto max-w-screen-xl"])}>
