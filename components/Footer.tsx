76779863f5ea97ebfcdfe780d0480a6ace2e0753
lvidal1 <lvidal910@gmail.com>
2023-01-05 22:00:44 -0500

    Fix lint errors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,14 +7,14 @@ const Footer = () => {
         <div className={styles.container}>
             <div className={styles.disclaimer}>
                 <p className='text-xl'><span className='font-bold'>Leonardo</span> Vidal</p>
-                <span className='text-xs text-slate-300'>{new Date().getFullYear()} | Inspired on <a target="_blank" href="https://dribbble.com/shots/11276631-DS-Personal-Developer-Portfolio">Logan Cee's template</a>
+                <span className='text-xs text-slate-300'>{new Date().getFullYear()} | Inspired on <a rel="noreferrer" target="_blank" href="https://dribbble.com/shots/11276631-DS-Personal-Developer-Portfolio">Logan Cee&apos;s template</a>
                 </span>
             </div>
             <div className={styles.socials}>
-                <SocialIcon target="_blank" url="https://www.linkedin.com/in/lvidalio/" bgColor='transparent' fgColor="white" network='linkedin' />
-                <SocialIcon target="_blank" url="https://twitter.com/lvidalio/" bgColor='transparent' fgColor="white" network='twitter' />
-                <SocialIcon target="_blank" url="https://www.instagram.com/lvidal.pe/" bgColor='transparent' fgColor="white" network='instagram' />
-                <SocialIcon target="_blank" url="https://github.com/lvidal1/" bgColor='transparent' fgColor="white" network='github' />
+                <SocialIcon rel="noreferrer" target="_blank" url="https://www.linkedin.com/in/lvidalio/" bgColor='transparent' fgColor="white" network='linkedin' />
+                <SocialIcon rel="noreferrer" target="_blank" url="https://twitter.com/lvidalio/" bgColor='transparent' fgColor="white" network='twitter' />
+                <SocialIcon rel="noreferrer" target="_blank" url="https://www.instagram.com/lvidal.pe/" bgColor='transparent' fgColor="white" network='instagram' />
+                <SocialIcon rel="noreferrer" target="_blank" url="https://github.com/lvidal1/" bgColor='transparent' fgColor="white" network='github' />
             </div>
         </div>
     </footer>
