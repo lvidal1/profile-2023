0081bd28b57e52f44641af87da8b5d898d895dc4
lvidal1 <lvidal910@gmail.com>
2023-01-06 21:25:00 -0500

    Move styles to CSS module

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -9,10 +9,10 @@ export const Introduction = () => {
 
                 </div>
                 <div className={styles.speech}>
-                    <div className='text-left text-xs  text-gray-400'>Introduce</div>
-                    <h4 className='text-left text-3xl md:text-4xl md:leading-snug'>Hello, I'm Leonardo Vidal</h4>
-                    <div className='text-left text-lg md:text-xl italic'>Great power comes with great responsability</div>
-                    <p className='text-left text-sm leading-loose text-gray-400'>
+                    <div className={styles.label}>Introduce</div>
+                    <h4 className={styles.title}>Hello, I'm Leonardo Vidal</h4>
+                    <div className={styles.quote}>Great power comes with great responsability</div>
+                    <p className={styles.description}>
                         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
                     </p>
                 </div>
