60412155f66951c1d5ce8ef93918f0f7555cac67
lvidal1 <lvidal910@gmail.com>
2023-03-14 23:41:39 -0500

    Add hyphen

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -19,7 +19,7 @@ export const Introduction = () => {
                 </div>
                 <div className={styles.speech}>
                     <div className={styles.label}>Introduction</div>
-                    <h4 className={styles.title}>Hola, I&apos;m Leonardo Vidal</h4>
+                    <h4 className={styles.title}>Hola, I&apos;m <span className={styles.hypen}>Leonardo</span> Vidal</h4>
                     <div className={styles.quote}>Crafting innovative web experiences to achieve your business goals.</div>
                     <p className={styles.description}>
                         This is me, a web developer with years of experience under my belt. From developing cutting-edge applications with the latest technologies to effectively leading teams through each stage of the web development cycle, I&apos;m confident that I have what it takes to bring your web development project to life. Let&apos;s work together to create scalable and high-quality products that exceed your expectations!
