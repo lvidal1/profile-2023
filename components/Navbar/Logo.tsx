ea843e2edccc60166ea3133668fb796cd74fe485
lvidal1 <lvidal910@gmail.com>
2023-03-17 01:13:59 -0500

    Add logo

--- components/Navbar/Logo.tsx
+++ components/Navbar/Logo.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
+
+
 
 export const Logo = () => {
     return (
-        <a href="https://flowbite.com/" className="flex items-center">
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">LV</span>
+        <a href="#" className="flex items-center relative w-7 h-7">
+            <Image
+                src="/logo.svg"
+                alt="Picture of the author"
+                fill
+            />
+
         </a>
     )
 }
