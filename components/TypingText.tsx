b2542ab6f0b12dfb32ed99ab457ad18a2986bd2e
lvidal1 <lvidal910@gmail.com>
2023-03-16 21:54:06 -0500

    Optimize typing text comp

--- components/TypingText.tsx
+++ components/TypingText.tsx
@@ -1,56 +1,40 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { TypeAnimation } from 'react-type-animation'
 import useTranslation from 'next-translate/useTranslation';
+import { locales, LOCALES_NAMESPACE } from '../utils/locale';
 
 export const TypingText = () => {
     const { t, lang } = useTranslation();
 
-    const sequenceEN = [
-        t('common:hero.typing.showCode'),
-        1000,
-        t('common:hero.typing.showDesign'),
-        1000,
-        t('common:hero.typing.showWeb'),
-        1000,
-        t('common:hero.typing.showAPI'),
-        1000,
-        t('common:hero.typing.showDB'),
-        1000,
-    ];
+    const getSequence = useCallback((lang: string) => {
+        const locale = LOCALES_NAMESPACE[lang]
+        return [
+            t(`${locale}:hero.typing.showCode`),
+            1000,
+            t(`${locale}:hero.typing.showDesign`),
+            1000,
+            t(`${locale}:hero.typing.showWeb`),
+            1000,
+            t(`${locale}:hero.typing.showAPI`),
+            1000,
+            t(`${locale}:hero.typing.showDB`),
+            1000,
+        ]
+    }, [lang])
 
-    const sequenceES = [
-        t('commonES:hero.typing.showCode'),
-        1000,
-        t('commonES:hero.typing.showDesign'),
-        1000,
-        t('commonES:hero.typing.showWeb'),
-        1000,
-        t('commonES:hero.typing.showAPI'),
-        1000,
-        t('commonES:hero.typing.showDB'),
-        1000,
-    ]
+    return <>
+        {locales.map((l) => {
+            return <>
+                {lang === l &&
+                    <TypeAnimation
+                        sequence={getSequence(l)}
+                        speed={45}
+                        wrapper="span"
+                        repeat={Infinity}
+                        cursor={true}
+                    />}
+            </>
+        })}
+    </>
 
-    return (
-        <>
-            {lang === "en" &&
-                <TypeAnimation
-                    // Same String at the start will only be typed once, initially
-                    sequence={sequenceEN}
-                    speed={45} // Custom Speed from 1-99 - Default Speed: 40
-                    wrapper="span" // Animation will be rendered as a <span>
-                    repeat={Infinity} // Repeat this Animation Sequence infinitely
-                    cursor={true}
-                />}
-            {lang === "es" &&
-                <TypeAnimation
-                    // Same String at the start will only be typed once, initially
-                    sequence={sequenceES}
-                    speed={45} // Custom Speed from 1-99 - Default Speed: 40
-                    wrapper="span" // Animation will be rendered as a <span>
-                    repeat={Infinity} // Repeat this Animation Sequence infinitely
-                    cursor={true}
-                />}
-        </>
-    )
 }
