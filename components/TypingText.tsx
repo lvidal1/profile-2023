e5183ad1f04b16033aa906ec92507e5e57aa0c94
lvidal1 <lvidal910@gmail.com>
2023-03-16 21:55:03 -0500

    Add key to map

--- components/TypingText.tsx
+++ components/TypingText.tsx
@@ -24,7 +24,7 @@ export const TypingText = () => {
 
     return <>
         {locales.map((l) => {
-            return <>
+            return <React.Fragment key={l}>
                 {lang === l &&
                     <TypeAnimation
                         sequence={getSequence(l)}
@@ -33,8 +33,7 @@ export const TypingText = () => {
                         repeat={Infinity}
                         cursor={true}
                     />}
-            </>
+            </React.Fragment>
         })}
     </>
-
 }
